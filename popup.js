04cdffbbaab87746f60139481a25b51c779730c0
Luke-ZL <33585119+Luke-ZL@users.noreply.github.com>
2019-03-31 01:37:55 -0400

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,10 @@
+'use strict'
+
+ let sub = document.getElementById('sub');
+ sub.onclick = function(){
+ 	var x = document.getElementById("URL").value;
+	var y = document.getElementById("N_hours").value;
+  	//document.getElementById("display").innerHTML = x;
+	console.log(x);
+	console.log(y);
+ }
